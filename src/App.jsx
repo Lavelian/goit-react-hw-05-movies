01935d67317e0281b2fd6db6608e0d8deae8e3b5
Lavelian <98335265+Lavelian@users.noreply.github.com>
2022-10-12 21:57:03 +0300

    add lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,47 @@
 import { Routes, Route } from 'react-router-dom';
-// import { lazy, Suspense } from 'react';
+import { lazy, Suspense } from 'react';
 import NavBar from 'components/MainNavBar';
 import Box from 'components/Box';
+import Modal from 'components/Modal';
+import Loader from 'components/Loader';
+
 // import Loader from 'components/Loader';
-import Home from 'pages/Home';
-import Movies from 'pages/Movies';
-import MovieDetails from './pages/MovieDetails';
-import Cast from 'pages/Cast';
-import Reviews from 'pages/Reviews';
-// const Home = lazy(() => import('pages/Home'));
-// const Movies = lazy(() => import('pages/Movies'));
-// const MovieDetails = lazy(() => import('pages/MovieDetails'));
-// const Cast = lazy(() => import('pages/Cast'));
-// const Reviews = lazy(() => import('pages/Reviews'));
+// import Home from 'pages/Home';
+// import Movies from 'pages/Movies';
+// import MovieDetails from './pages/MovieDetails';
+// import Cast from 'pages/Cast';
+// import Reviews from 'pages/Reviews';
+const Home = lazy(() => import('pages/Home'));
+const Movies = lazy(() => import('pages/Movies'));
+const MovieDetails = lazy(() => import('pages/MovieDetails'));
+const Cast = lazy(() => import('pages/Cast'));
+const Reviews = lazy(() => import('pages/Reviews'));
 
 export const App = () => {
   return (
     <>
-      {/* <Suspense fallback={<Loader />}></Suspense> */}
-      <Box as="header" mb={2} borderBottom="normal" borderBottomColor="black">
-        <NavBar />
-      </Box>
-      <Box width="1200px" px={1} mx="auto">
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="movies/:movieId" element={<MovieDetails />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
-          </Route>
-          <Route path="*" element={<>Page not found</>} />
-        </Routes>
-      </Box>
+      <Suspense
+        fallback={
+          <Modal>
+            <Loader />
+          </Modal>
+        }
+      >
+        <Box as="header" mb={2} borderBottom="normal" borderBottomColor="black">
+          <NavBar />
+        </Box>
+        <Box width="1200px" px={1} mx="auto">
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="movies/:movieId" element={<MovieDetails />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
+            <Route path="*" element={<>Page not found</>} />
+          </Routes>
+        </Box>
+      </Suspense>
     </>
   );
 };
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,44 +1,21 @@
 import { Oval } from 'react-loader-spinner';
 import { LoaderStyled } from './Loder.styles';
-import { createPortal } from 'react-dom';
-import Modal from 'components/Modal';
-
 const loader = document.querySelector('#loader');
+console.log(loader);
 
 const Loader = () => {
-  return createPortal(
-    <Modal>
-      <LoaderStyled>
-        <Oval
-          ariaLabel="loading-indicator"
-          height={100}
-          width={100}
-          strokeWidth={5}
-          strokeWidthSecondary={1}
-          color="blue"
-          secondaryColor="white"
-        />
-      </LoaderStyled>
-    </Modal>,
-    loader
+  return (
+    <LoaderStyled>
+      <Oval
+        ariaLabel="loading-indicator"
+        height={100}
+        width={100}
+        strokeWidth={5}
+        strokeWidthSecondary={1}
+        color="blue"
+        secondaryColor="white"
+      />
+    </LoaderStyled>
   );
 };
 export default Loader;
-
-// export const Loader = props => {
-//   return createPortal(
-//     <div className="Overlay">
-//       <ThreeDots
-//         height="80"
-//         width="80"
-//         radius="9"
-//         color="#ff5100"
-//         ariaLabel="three-dots-loading"
-//         wrapperStyle={{}}
-//         wrapperClassName=""
-//         visible={true}
-//       />
-//     </div>,
-//     modalPortal
-//   );
-// };
